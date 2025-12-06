8e8c625383841a86b7b901d58fa34d6632faea53
Ani <5357586+anistark@users.noreply.github.com>
2025-12-06 17:20:57 +0530

    fix: sot url

--- Formula/sot.rb
+++ Formula/sot.rb
@@ -3,59 +3,59 @@ class Sot < Formula
 
   desc "Command-line System Observation Tool"
   homepage "https://github.com/anistark/sot"
-  url "https://files.pythonhosted.org/packages/a2/ab/47857ca85332e2afe9fd54e439f51   b2c6fe54e37c38ec951a689e369047d/sot-5.0.0.tar.gz"
+  url "https://files.pythonhosted.org/packages/a2/ab/47857ca85332e2afe9fd54e439f51b2c6fe54e37c38ec951a689e369047d/sot-5.0.0.tar.gz"
   sha256 "a9dc126a6b4c7edb7d516f0ab06f7d4bba04c4848c6459b584c293c61124f680"
   license "MIT"
 
   depends_on "python@3.12"
 
   resource "py-cpuinfo" do
-    url "https://files.pythonhosted.org/packages/a2/ab/47857ca85332e2afe9fd54e439f51   b2c6fe54e37c38ec951a689e369047d/sot-5.0.0.tar.gz"
+    url "https://files.pythonhosted.org/packages/a2/ab/47857ca85332e2afe9fd54e439f51b2c6fe54e37c38ec951a689e369047d/sot-5.0.0.tar.gz"
     sha256 "a9dc126a6b4c7edb7d516f0ab06f7d4bba04c4848c6459b584c293c61124f680"
   end
 
   resource "distro" do
-    url "https://files.pythonhosted.org/packages/a2/ab/47857ca85332e2afe9fd54e439f51   b2c6fe54e37c38ec951a689e369047d/sot-5.0.0.tar.gz"
+    url "https://files.pythonhosted.org/packages/a2/ab/47857ca85332e2afe9fd54e439f51b2c6fe54e37c38ec951a689e369047d/sot-5.0.0.tar.gz"
     sha256 "a9dc126a6b4c7edb7d516f0ab06f7d4bba04c4848c6459b584c293c61124f680"
   end
 
   resource "psutil" do
-    url "https://files.pythonhosted.org/packages/a2/ab/47857ca85332e2afe9fd54e439f51   b2c6fe54e37c38ec951a689e369047d/sot-5.0.0.tar.gz"
+    url "https://files.pythonhosted.org/packages/a2/ab/47857ca85332e2afe9fd54e439f51b2c6fe54e37c38ec951a689e369047d/sot-5.0.0.tar.gz"
     sha256 "a9dc126a6b4c7edb7d516f0ab06f7d4bba04c4848c6459b584c293c61124f680"
   end
 
   resource "rich" do
-    url "https://files.pythonhosted.org/packages/a2/ab/47857ca85332e2afe9fd54e439f51   b2c6fe54e37c38ec951a689e369047d/sot-5.0.0.tar.gz"
+    url "https://files.pythonhosted.org/packages/a2/ab/47857ca85332e2afe9fd54e439f51b2c6fe54e37c38ec951a689e369047d/sot-5.0.0.tar.gz"
     sha256 "a9dc126a6b4c7edb7d516f0ab06f7d4bba04c4848c6459b584c293c61124f680"
   end
 
   resource "textual" do
-    url "https://files.pythonhosted.org/packages/a2/ab/47857ca85332e2afe9fd54e439f51   b2c6fe54e37c38ec951a689e369047d/sot-5.0.0.tar.gz"
+    url "https://files.pythonhosted.org/packages/a2/ab/47857ca85332e2afe9fd54e439f51b2c6fe54e37c38ec951a689e369047d/sot-5.0.0.tar.gz"
     sha256 "a9dc126a6b4c7edb7d516f0ab06f7d4bba04c4848c6459b584c293c61124f680"
   end
 
   resource "markdown-it-py" do
-    url "https://files.pythonhosted.org/packages/a2/ab/47857ca85332e2afe9fd54e439f51   b2c6fe54e37c38ec951a689e369047d/sot-5.0.0.tar.gz"
+    url "https://files.pythonhosted.org/packages/a2/ab/47857ca85332e2afe9fd54e439f51b2c6fe54e37c38ec951a689e369047d/sot-5.0.0.tar.gz"
     sha256 "a9dc126a6b4c7edb7d516f0ab06f7d4bba04c4848c6459b584c293c61124f680"
   end
 
   resource "mdurl" do
-    url "https://files.pythonhosted.org/packages/a2/ab/47857ca85332e2afe9fd54e439f51   b2c6fe54e37c38ec951a689e369047d/sot-5.0.0.tar.gz"
+    url "https://files.pythonhosted.org/packages/a2/ab/47857ca85332e2afe9fd54e439f51b2c6fe54e37c38ec951a689e369047d/sot-5.0.0.tar.gz"
     sha256 "a9dc126a6b4c7edb7d516f0ab06f7d4bba04c4848c6459b584c293c61124f680"
   end
 
   resource "pygments" do
-    url "https://files.pythonhosted.org/packages/a2/ab/47857ca85332e2afe9fd54e439f51   b2c6fe54e37c38ec951a689e369047d/sot-5.0.0.tar.gz"
+    url "https://files.pythonhosted.org/packages/a2/ab/47857ca85332e2afe9fd54e439f51b2c6fe54e37c38ec951a689e369047d/sot-5.0.0.tar.gz"
     sha256 "a9dc126a6b4c7edb7d516f0ab06f7d4bba04c4848c6459b584c293c61124f680"
   end
 
   resource "typing-extensions" do
-    url "https://files.pythonhosted.org/packages/a2/ab/47857ca85332e2afe9fd54e439f51   b2c6fe54e37c38ec951a689e369047d/sot-5.0.0.tar.gz"
+    url "https://files.pythonhosted.org/packages/a2/ab/47857ca85332e2afe9fd54e439f51b2c6fe54e37c38ec951a689e369047d/sot-5.0.0.tar.gz"
     sha256 "a9dc126a6b4c7edb7d516f0ab06f7d4bba04c4848c6459b584c293c61124f680"
   end
 
   resource "platformdirs" do
-    url "https://files.pythonhosted.org/packages/a2/ab/47857ca85332e2afe9fd54e439f51   b2c6fe54e37c38ec951a689e369047d/sot-5.0.0.tar.gz"
+    url "https://files.pythonhosted.org/packages/a2/ab/47857ca85332e2afe9fd54e439f51b2c6fe54e37c38ec951a689e369047d/sot-5.0.0.tar.gz"
     sha256 "a9dc126a6b4c7edb7d516f0ab06f7d4bba04c4848c6459b584c293c61124f680"
   end
 
