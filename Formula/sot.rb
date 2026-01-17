b5be7d51cb08b45c31525de6a5e3cf0e4cc5dd11
Kumar Anirudha <mail@anirudha.dev>
2026-01-18 00:52:24 +0530

    fix: sot v6.0.0 formula fix

--- Formula/sot.rb
+++ Formula/sot.rb
@@ -10,53 +10,53 @@ class Sot < Formula
   depends_on "python@3.12"
 
   resource "py-cpuinfo" do
-    url "https://files.pythonhosted.org/packages/c0/e3/eb84c0e0240a5a9bc2a2eb7e369d1eba2c6b10dec97f87a20e7c1cd27b72/sot-6.0.0.tar.gz"
-    sha256 "bc684d916df26d1c0bc9adfe29352ae84baf1601cd2fb284f6492e9e3f951ea3"
+    url "https://files.pythonhosted.org/packages/37/a8/d832f7293ebb21690860d2e01d8115e5ff6f2ae8bbdc953f0eb0fa4bd2c7/py-cpuinfo-9.0.0.tar.gz"
+    sha256 "3cdbbf3fac90dc6f118bfd64384f309edeadd902d7c8fb17f02ffa1fc3f49690"
   end
 
   resource "distro" do
-    url "https://files.pythonhosted.org/packages/c0/e3/eb84c0e0240a5a9bc2a2eb7e369d1eba2c6b10dec97f87a20e7c1cd27b72/sot-6.0.0.tar.gz"
-    sha256 "bc684d916df26d1c0bc9adfe29352ae84baf1601cd2fb284f6492e9e3f951ea3"
+    url "https://files.pythonhosted.org/packages/fc/f8/98eea607f65de6527f8a2e8885fc8015d3e6f5775df186e443e0964a11c3/distro-1.9.0.tar.gz"
+    sha256 "2fa77c6fd8940f116ee1d6b94a2f90b13b5ea8d019b98bc8bafdcabcdd9bdbed"
   end
 
   resource "psutil" do
-    url "https://files.pythonhosted.org/packages/c0/e3/eb84c0e0240a5a9bc2a2eb7e369d1eba2c6b10dec97f87a20e7c1cd27b72/sot-6.0.0.tar.gz"
-    sha256 "bc684d916df26d1c0bc9adfe29352ae84baf1601cd2fb284f6492e9e3f951ea3"
+    url "https://files.pythonhosted.org/packages/73/cb/09e5184fb5fc0358d110fc3ca7f6b1d033800734d34cac10f4136cfac10e/psutil-7.2.1.tar.gz"
+    sha256 "f7583aec590485b43ca601dd9cea0dcd65bd7bb21d30ef4ddbf4ea6b5ed1bdd3"
   end
 
   resource "rich" do
-    url "https://files.pythonhosted.org/packages/c0/e3/eb84c0e0240a5a9bc2a2eb7e369d1eba2c6b10dec97f87a20e7c1cd27b72/sot-6.0.0.tar.gz"
-    sha256 "bc684d916df26d1c0bc9adfe29352ae84baf1601cd2fb284f6492e9e3f951ea3"
+    url "https://files.pythonhosted.org/packages/fb/d2/8920e102050a0de7bfabeb4c4614a49248cf8d5d7a8d01885fbb24dc767a/rich-14.2.0.tar.gz"
+    sha256 "73ff50c7c0c1c77c8243079283f4edb376f0f6442433aecb8ce7e6d0b92d1fe4"
   end
 
   resource "textual" do
-    url "https://files.pythonhosted.org/packages/c0/e3/eb84c0e0240a5a9bc2a2eb7e369d1eba2c6b10dec97f87a20e7c1cd27b72/sot-6.0.0.tar.gz"
-    sha256 "bc684d916df26d1c0bc9adfe29352ae84baf1601cd2fb284f6492e9e3f951ea3"
+    url "https://files.pythonhosted.org/packages/6f/ee/620c887bfad9d6eba062dfa3b6b0e735e0259102e2667b19f21625ef598d/textual-7.3.0.tar.gz"
+    sha256 "3169e8ba5518a979b0771e60be380ab1a6c344f30a2126e360e6f38d009a3de4"
   end
 
   resource "markdown-it-py" do
-    url "https://files.pythonhosted.org/packages/c0/e3/eb84c0e0240a5a9bc2a2eb7e369d1eba2c6b10dec97f87a20e7c1cd27b72/sot-6.0.0.tar.gz"
-    sha256 "bc684d916df26d1c0bc9adfe29352ae84baf1601cd2fb284f6492e9e3f951ea3"
+    url "https://files.pythonhosted.org/packages/5b/f5/4ec618ed16cc4f8fb3b701563655a69816155e79e24a17b651541804721d/markdown_it_py-4.0.0.tar.gz"
+    sha256 "cb0a2b4aa34f932c007117b194e945bd74e0ec24133ceb5bac59009cda1cb9f3"
   end
 
   resource "mdurl" do
-    url "https://files.pythonhosted.org/packages/c0/e3/eb84c0e0240a5a9bc2a2eb7e369d1eba2c6b10dec97f87a20e7c1cd27b72/sot-6.0.0.tar.gz"
-    sha256 "bc684d916df26d1c0bc9adfe29352ae84baf1601cd2fb284f6492e9e3f951ea3"
+    url "https://files.pythonhosted.org/packages/d6/54/cfe61301667036ec958cb99bd3efefba235e65cdeb9c84d24a8293ba1d90/mdurl-0.1.2.tar.gz"
+    sha256 "bb413d29f5eea38f31dd4754dd7377d4465116fb207585f97bf925588687c1ba"
   end
 
   resource "pygments" do
-    url "https://files.pythonhosted.org/packages/c0/e3/eb84c0e0240a5a9bc2a2eb7e369d1eba2c6b10dec97f87a20e7c1cd27b72/sot-6.0.0.tar.gz"
-    sha256 "bc684d916df26d1c0bc9adfe29352ae84baf1601cd2fb284f6492e9e3f951ea3"
+    url "https://files.pythonhosted.org/packages/b0/77/a5b8c569bf593b0140bde72ea885a803b82086995367bf2037de0159d924/pygments-2.19.2.tar.gz"
+    sha256 "636cb2477cec7f8952536970bc533bc43743542f70392ae026374600add5b887"
   end
 
   resource "typing-extensions" do
-    url "https://files.pythonhosted.org/packages/c0/e3/eb84c0e0240a5a9bc2a2eb7e369d1eba2c6b10dec97f87a20e7c1cd27b72/sot-6.0.0.tar.gz"
-    sha256 "bc684d916df26d1c0bc9adfe29352ae84baf1601cd2fb284f6492e9e3f951ea3"
+    url "https://files.pythonhosted.org/packages/72/94/1a15dd82efb362ac84269196e94cf00f187f7ed21c242792a923cdb1c61f/typing_extensions-4.15.0.tar.gz"
+    sha256 "0cea48d173cc12fa28ecabc3b837ea3cf6f38c6d1136f85cbaaf598984861466"
   end
 
   resource "platformdirs" do
-    url "https://files.pythonhosted.org/packages/c0/e3/eb84c0e0240a5a9bc2a2eb7e369d1eba2c6b10dec97f87a20e7c1cd27b72/sot-6.0.0.tar.gz"
-    sha256 "bc684d916df26d1c0bc9adfe29352ae84baf1601cd2fb284f6492e9e3f951ea3"
+    url "https://files.pythonhosted.org/packages/cf/86/0248f086a84f01b37aaec0fa567b397df1a119f73c16f6c7a9aac73ea309/platformdirs-4.5.1.tar.gz"
+    sha256 "61d5cdcc6065745cdd94f0f878977f8de9437be93de97c1c12f853c9c0cdcbda"
   end
 
   def install
